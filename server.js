aef96c1ff0f5c6803d0e07d2e886c34e9dac4221
hi-kaye <kayehin@gmail..com>
2020-10-15 10:56:57 +0100

    adding server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const app = express()
+const {User, Tasks, Board, AdminTable, sequelize} = require('./models')
+
+const handlebars = expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+app.use(express.static('public'))
+app.engine('handlebars', handlebars)
+app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//landing page
+app.get('/', (req, res) => {
+    res.render('landing', {layout : 'main'});
+    })
+
+//app is listening on port 3000
+app.listen(3000, async() => {
+    await sequelize.sync()
+    console.log('Web server is running')
+})
