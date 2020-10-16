06ce2bc631412a25690142134b6931c20cd54a3a
Keratuwe <mokhantsopheko@gmail.com>
2020-10-16 13:29:33 +0100

    new changes

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const Handlebars = require('handlebars')
 const expressHandlebars = require('express-handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const app = express()
-const {User, Task, Board, AdminTable, sequelize} = require('./models')
+const {User, Task, Board, AdminTable, sequelize, Op} = require('./models')
 const e = require('express')
 var loggedIndex;
+
 
 const handlebars = expressHandlebars({
     handlebars: allowInsecurePrototypeAccess(Handlebars)
@@ -18,19 +19,21 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 //landing page
+
 app.get('/', (req, res) => {
     res.render('landing', {layout : 'main'});
     })
 
-/*-----------------------Login-Create----------------------*/
+/*-----------------------Login-Create Render----------------------*/
 app.get('/login-create', (req, res) => 
 {
     res.render('login-create', {layout : 'main'});
 })
 
   
-//login-create page login
-app.post('/login-create', async (req, res) =>{
+/*--------------------------------login-create login validation--------------------------------*/
+app.post('/login', async (req, res) =>
+{
     console.log(req.body);
     const users = await User.findAll();
     let isFound = false;
@@ -51,8 +54,10 @@ app.post('/login-create', async (req, res) =>{
         }
 })
 
-//login-create page sign-up
-app.post('/login-create', async (req, res) => {
+
+/*-------------------------login-create sign-up validation---------------------------*/
+app.post('/signup', async (req, res) =>
+{
    
     const users = await User.findAll();
     let isFound = false;
@@ -71,10 +76,60 @@ app.post('/login-create', async (req, res) => {
     }
 
     res.redirect('/login-create')
+})
+
+
+/*-----------------------my boards page--------------------------*/ 
+app.get('/myBoards', async (req, res) => {
+    const users = await User.findAll({
+        where: {
+            id: {
+                [Op.ne]: loggedIndex
+            }
+        }
     })
+    
+    const admin = await AdminTable.findAll({
+        where: {
+          UserId: loggedIndex
+        }
+      })
+    
+    const boards = await Promise.all(admin.map(admin => Board.findByPk(admin.BoardId)))
+
+    res.render('myBoards', {users, boards})
+})
+
+/*-----------------------create new board--------------------------*/
+
+app.post('/myBoards', async (req, res) => {
+    const data = req.body
+    console.log(data)
+    const newBoard = await Board.create({title: data.title, image: data.image})
+    await AdminTable.create({UserId: loggedIndex, BoardId: newBoard.id})
+
+    if (data.users.length == 1) {
+        await AdminTable.create({UserId: data.users, BoardId: newBoard.id})
+
+    }
+    else {
+        (data.users).forEach(async (user) => {
+        await AdminTable.create({UserId: user, BoardId: newBoard.id})
+        })
+    }
+
+    res.redirect('/myBoards')
+})
+
+/*---------------------All Boards (Explore)-----------------------*/
+app.get('/explore', async (req, res) => 
+{
+    const boards = await Board.findAll()
+    res.render('explore', {boards});
+})
 
 
-//app is listening on port 3000
+/*---------------------app is listening on port 3000-----------------------*/
 app.listen(3000, async() => {
     await sequelize.sync()
     console.log('Web server is running')
