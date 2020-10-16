ca661976762be8e23ff167d3646329ec48b3514c
Keratuwe <mokhantsopheko@gmail.com>
2020-10-16 10:31:04 +0100

    login-create.handlebars server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const expressHandlebars = require('express-handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const app = express()
 const {User, Task, Board, AdminTable, sequelize} = require('./models')
+const e = require('express')
+var loggedIndex;
 
 const handlebars = expressHandlebars({
     handlebars: allowInsecurePrototypeAccess(Handlebars)
@@ -19,6 +21,58 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.render('landing', {layout : 'main'});
     })
+
+/*-----------------------Login-Create----------------------*/
+app.get('/login-create', (req, res) => 
+{
+    res.render('login-create', {layout : 'main'});
+})
+
+  
+//login-create page login
+app.post('/login-create', async (req, res) =>{
+    console.log(req.body);
+    const users = await User.findAll();
+    let isFound = false;
+    users.forEach(user => 
+        {
+            if(user.username == req.body.username)
+            {
+                isFound = true;
+                loggedIndex = user.id;
+                console.log(loggedIndex);
+                res.redirect('/myBoards')
+            } 
+        })
+        
+        if(isFound == false)
+        {
+            res.redirect('/login-create');
+        }
+})
+
+//login-create page sign-up
+app.post('/login-create', async (req, res) => {
+   
+    const users = await User.findAll();
+    let isFound = false;
+    users.forEach(user => 
+    {
+        if(user.username == req.body.username)
+        {
+            isFound = true;
+            res.redirect('/login-create');
+        } 
+    })
+    
+    if(isFound == false)
+    {
+    await User.create(req.body)
+    }
+
+    res.redirect('/login-create')
+    })
+
 
 //app is listening on port 3000
 app.listen(3000, async() => {
